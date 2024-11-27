9fb05ddbedca7f7f82947605062af7fc22910676
Ivan Kikic <ivankikic49@gmail.com>
2024-11-27 19:20:41 +0100

    test fix

--- dinver-backend/seeders/20241127175719-demo-user.js
+++ dinver-backend/seeders/20241127175719-demo-user.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const hashedPassword = await bcrypt.hash('your_password_here', 10);
+    const hashedPassword = await bcrypt.hash('password', 10);
     return queryInterface.bulkInsert(
       'Users',
       [
