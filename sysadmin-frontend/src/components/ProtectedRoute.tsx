30337d550698cca7784ab7b9b048684f7862175b
Ivan Kikic <ivankikic49@gmail.com>
2025-01-17 20:11:10 +0100

    Add loading spinner

--- sysadmin-frontend/src/components/ProtectedRoute.tsx
+++ sysadmin-frontend/src/components/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import LoadingSpinner from "./LoadingSpinner";
 
 const ProtectedRoute = () => {
   const { isAuthenticated, isLoading } = useAuth();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <LoadingSpinner />;
   }
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
