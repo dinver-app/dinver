5a447fc7e9d624a96333a0d2da233edbdb902beb
Ivan Kikic <ivankikic49@gmail.com>
2025-02-12 15:58:53 +0100

    Custom homepage

--- sysadmin-frontend/src/pages/Home.tsx
+++ sysadmin-frontend/src/pages/Home.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 const Home = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className="max-w-md mx-auto mt-10">
-      <h1 className="text-2xl font-bold mb-4">Welcome to the Dashboard</h1>
-      <p>This is the homepage. You can add more components here.</p>
+    <div className="w-full h-full flex flex-col items-center justify-center mt-36">
+      <h1 className="text-3xl font-bold mb-4">{t("welcome_message")}</h1>
+      <p className="text-lg">{t("homepage_description")}</p>
     </div>
   );
 };
