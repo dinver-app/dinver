ffdb1c6ae4438d48737b7d0df31fb0c108f53887
Ivan Kikic <ivankikic49@gmail.com>
2025-02-15 14:54:09 +0100

    Change structure of Analytics page

--- sysadmin-frontend/src/pages/Analytics.tsx
+++ sysadmin-frontend/src/pages/Analytics.tsx
@@ -1,8 +1,12 @@
 const Analytics = () => {
   return (
-    <div className="max-w-md mx-auto mt-10">
-      <h1 className="text-2xl font-bold mb-4">Analytics</h1>
-      <p>Manage your analytics here.</p>
+    <div className="mx-auto p-4">
+      <div className="flex flex-col justify-between items-start mb-4">
+        <h1 className="page-title">Analytics</h1>
+        <h3 className="page-subtitle">Manage your analytics here.</h3>
+      </div>
+      <div className="h-line mb-4"></div>
+      <div className="flex justify-between items-center mb-4">TBD</div>
     </div>
   );
 };
