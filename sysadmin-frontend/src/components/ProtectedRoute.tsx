1ee101bc9d19d537fc2a0279d1fe5fc256a71bcf
Ivan Kikic <ivankikic49@gmail.com>
2025-02-19 14:31:58 +0100

    Test protectedRoute bug

--- sysadmin-frontend/src/components/ProtectedRoute.tsx
+++ sysadmin-frontend/src/components/ProtectedRoute.tsx
@@ -13,10 +13,11 @@ const ProtectedRoute = () => {
       setIsLoading(true);
       try {
         const response = await checkAuth();
+        console.log(response);
         setIsAuthenticated(response.isAuthenticated);
       } catch (error) {
-        setIsAuthenticated(false);
-        window.location.href = "/login";
+        // setIsAuthenticated(false);
+        // window.location.href = "/login";
       } finally {
         setIsLoading(false);
       }
