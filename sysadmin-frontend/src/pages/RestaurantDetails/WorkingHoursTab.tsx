cf11814fa41eeb6f099dbb09cd57a1f0a02c4133
Ivan Kikic <ivankikic49@gmail.com>
2025-02-22 23:42:30 +0100

    Fix custom date

--- sysadmin-frontend/src/pages/RestaurantDetails/WorkingHoursTab.tsx
+++ sysadmin-frontend/src/pages/RestaurantDetails/WorkingHoursTab.tsx
@@ -230,9 +230,12 @@ const WorkingHoursTab = ({ restaurant, onUpdate }: WorkingHoursTabProps) => {
       toast.success(t("custom_day_added_successfully"));
       setIsAddModalOpen(false);
       setNewCustomDay({ name: "", date: "", times: [{ open: "", close: "" }] });
-    } catch (error) {
-      console.error("Failed to add custom working day", error);
-      toast.error(t("failed_to_add_custom_day"));
+    } catch (error: any) {
+      console.error(
+        "Failed to add custom working day",
+        error.response.data.error
+      );
+      toast.error(t(error.response.data.error));
     }
   };
 
@@ -255,8 +258,9 @@ const WorkingHoursTab = ({ restaurant, onUpdate }: WorkingHoursTabProps) => {
       toast.success(t("custom_day_updated_successfully"));
       setIsEditModalOpen(false);
       setEditCustomDay(null);
-    } catch (error) {
+    } catch (error: any) {
       console.error("Failed to update custom working day", error);
+      toast.error(t(error.response.data.error));
     }
   };
 
