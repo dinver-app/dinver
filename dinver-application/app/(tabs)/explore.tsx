f0cedf584bc8946c2bb3e1aea51ead0390a3314a
Ivan Kikic <ivankikic49@gmail.com>
2025-03-06 16:05:03 +0100

    Init dinver-application

---
+++ dinver-application/app/(tabs)/explore.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const explore = () => {
+  return (
+    <View className="p-4 flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-white">Explore</Text>
+    </View>
+  );
+};
+
+export default explore;
---
+++ dinver-application/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="p-4 flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-white">
+        Moja Početna Stranica
+      </Text>
+    </View>
+  );
+}
