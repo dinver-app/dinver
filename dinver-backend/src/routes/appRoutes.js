b8ec60d7fdc0df04a4a55a006a910a93e35dd04e
Ivan Kikic <ivankikic49@gmail.com>
2025-03-06 21:22:54 +0100

    Fix backend bug

--- dinver-backend/src/routes/appRoutes.js
+++ dinver-backend/src/routes/appRoutes.js
@@ -1,20 +1,8 @@
-// const express = require('express');
-// const adminRoutes = require('./adminRoutes/adminRoutes');
-// const auditLogRoutes = require('./adminRoutes/auditLogRoutes');
-// const authRoutes = require('./adminRoutes/authRoutes');
-// const menuRoutes = require('./adminRoutes/menuRoutes');
-// const restaurantRoutes = require('./adminRoutes/restaurantRoutes');
-// const typeRoutes = require('./adminRoutes/typeRoutes');
-// const userRoutes = require('./adminRoutes/userRoutes');
+const express = require('express');
+const authRoutes = require('./appRoutes/authRoutes');
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.use(adminRoutes);
-// router.use(auditLogRoutes);
-// router.use(authRoutes);
-// router.use(menuRoutes);
-// router.use(restaurantRoutes);
-// router.use(typeRoutes);
-// router.use(userRoutes);
+router.use(authRoutes);
 
-// module.exports = router;
+module.exports = router;
