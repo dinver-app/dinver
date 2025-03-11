e9fd07733335186f14e90295ba53028600138d18
Ivan Kikic <ivankikic49@gmail.com>
2025-03-11 18:40:37 +0100

    Add app restaurant service

---
+++ dinver-backend/src/routes/appRoutes/restaurantRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const restaurantController = require('../../controllers/restaurantController');
+const { appAuthenticateToken } = require('../../middleware/roleMiddleware');
+
+const router = express.Router();
+
+router.get(
+  '/restaurants',
+  appAuthenticateToken,
+  restaurantController.getRestaurants,
+);
+
+router.get(
+  '/restaurants/all',
+  appAuthenticateToken,
+  restaurantController.getAllRestaurants,
+);
