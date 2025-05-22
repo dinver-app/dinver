4693f8e58de09d92e37757c828602253c941c419
Ivan Kikic <ivankikic49@gmail.com>
2025-05-22 08:25:48 +0200

    Remove auth requirement for trend search

--- dinver-backend/src/routes/appRoutes/trendingSearchesRoutes.js
+++ dinver-backend/src/routes/appRoutes/trendingSearchesRoutes.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const trendingSearchesController = require('../../controllers/trendingSearchesController');
-const {
-  appApiKeyAuth,
-  appAuthenticateToken,
-} = require('../../middleware/roleMiddleware');
+const { appApiKeyAuth } = require('../../middleware/roleMiddleware');
 
 const router = express.Router();
 
 router.get(
   '/trending-searches',
   appApiKeyAuth,
-  appAuthenticateToken,
   trendingSearchesController.getTrendingSearches,
 );
 
