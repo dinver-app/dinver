b0f64493a8e2dac6b2d761fd58f34dcb7132663e
Luka Bak <bakluka@gmail.com>
2025-12-05 21:30:37 +0100

    Add basic opentelemetry instrumentation

--- dinver-backend/src/server.js
+++ dinver-backend/src/server.js
@@ -2,11 +2,33 @@ const app = require('./app');
 const { Sequelize } = require('sequelize');
 const cron = require('node-cron');
 const leaderboardCycleManager = require('./cron/leaderboardCycleManager');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
+const { BatchLogRecordProcessor } = require('@opentelemetry/sdk-logs');
+const { resourceFromAttributes } = require('@opentelemetry/resources');
 
 const PORT = process.env.PORT || 3000;
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+
+if (env !== 'development')  {
+  const sdk = new NodeSDK({
+    resource: resourceFromAttributes({
+      'service.name': `dinver-backend-${env}`,
+    }),
+    logRecordProcessor: new BatchLogRecordProcessor(
+      new OTLPLogExporter({
+        url: 'https://eu.i.posthog.com/i/v1/logs',
+        headers: {
+          'Authorization': `Bearer ${process.env.POSTHOG_API_KEY}`
+        }
+      })
+    )
+  });
+  
+  sdk.start();
+}
 
 let sequelize;
 if (config.use_env_variable) {
