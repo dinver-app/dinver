62ab1a0bd85c3eaebdbfc0805aed16f6de3f705e
Luka Bak <bakluka@gmail.com>
2025-12-05 23:07:30 +0100

    Extract instrumentation to separate file

--- dinver-backend/src/server.js
+++ dinver-backend/src/server.js
@@ -2,45 +2,12 @@ const app = require('./app');
 const { Sequelize } = require('sequelize');
 const cron = require('node-cron');
 const leaderboardCycleManager = require('./cron/leaderboardCycleManager');
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
-const { BatchLogRecordProcessor } = require('@opentelemetry/sdk-logs');
-const { resourceFromAttributes } = require('@opentelemetry/resources');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
 const PORT = process.env.PORT || 3000;
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-
-if (env !== 'development') {
-  const sdk = new NodeSDK({
-    resource: resourceFromAttributes({
-      'service.name': `dinver-backend-${env}`,
-    }),
-    logRecordProcessor: new BatchLogRecordProcessor(
-      new OTLPLogExporter({
-        url: 'https://eu.i.posthog.com/i/v1/logs',
-        headers: {
-          Authorization: `Bearer ${process.env.POSTHOG_API_KEY}`,
-        },
-      }),
-    ),
-    traceExporter: new OTLPTraceExporter({
-      url: 'https://eu.i.posthog.com/v1/logs',
-      headers: {
-        Authorization: `Bearer ${process.env.POSTHOG_API_KEY}`,
-      },
-    }),
-    instrumentations: getNodeAutoInstrumentations({
-      '@opentelemetry/instrumentation-express': { enabled: true },
-      '@opentelemetry/instrumentation-http': { enabled: true },
-    }),
-  });
-
-  sdk.start();
-}
+const { logs } = require('@opentelemetry/api-logs');
 
 let sequelize;
 if (config.use_env_variable) {
@@ -66,5 +33,14 @@ sequelize.sync().then(() => {
     });
 
     console.log('Leaderboard cycle cron job scheduled to run every hour');
+
+    const logger = logs.getLogger('logger-first');
+    logger.emit(
+      {
+        severityText: 'info',
+        body: `Application started successfully (${new Date().toISOString()}) `,
+        attributes: {port: PORT, environment: env}
+      }
+    );
   });
 });
